ea59ca77ca737d6e316251b979c738a3efe4677e
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-08 12:34:13 +0100

    refactor: apply one point per line in 'HttpResponse.toString'

--- src/main/java/HttpResponse.java
+++ src/main/java/HttpResponse.java
@@ -14,11 +14,21 @@ public class HttpResponse {
     @Override
     public String toString() {
         StringBuilder response = new StringBuilder();
-        response.append("HTTP/1.1 ").append(statusCode).append(" ").append(responseBody).append(System.lineSeparator());
-        response.append("Content-Type: text/plain").append(System.lineSeparator());
-        response.append("Content-Length: ").append(responseBody.length()).append(System.lineSeparator());
+        response.append("HTTP/1.1")
+                .append(" ")
+                .append(statusCode)
+                .append(" ")
+                .append(responseBody)
+                .append(System.lineSeparator());
+        response.append("Content-Type: text/plain")
+                .append(System.lineSeparator());
+        response.append("Content-Length:")
+                .append(" ")
+                .append(responseBody.length())
+                .append(System.lineSeparator());
         response.append(System.lineSeparator());
-        response.append(responseBody).append(System.lineSeparator());
+        response.append(responseBody)
+                .append(System.lineSeparator());
         return response.toString();
     }
 }

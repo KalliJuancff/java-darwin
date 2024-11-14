538651693fc88e8596d998ccc7ee5a658ca08620
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-14 17:22:10 +0100

    refactor: move routing code from tests to Main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        Application application = new Application();
-        application.run();
+        Application app = new Application();
+
+        app.get("/hello", (req, res) -> {
+            if (req.isPostMethod()) {
+                res.convertTo(HttpResponse.created());
+            } else {
+                res.convertTo(HttpResponse.ok());
+            }
+        });
+        app.get("/greet", (req, res) -> {
+            StringBuilder names = extractNamesFrom(req);
+            res.convertTo(HttpResponse.ok("Hi, " + names + "!"));
+        });
+
+        app.run();
+    }
+
+    private static StringBuilder extractNamesFrom(HttpRequest httpRequest) {
+        StringBuilder names = new StringBuilder();
+        for (QueryParameter queryParameter : httpRequest.queryParameters()) {
+            if (!names.toString().isEmpty()) {
+                names.append(" and ");
+            }
+            names.append(queryParameter.value());
+        }
+        return names;
     }
 }
--- src/test/java/DarwinShould.java
+++ src/test/java/DarwinShould.java
@@ -97,31 +97,6 @@ public class DarwinShould {
     }
 
     private static void runApplication() {
-        var app = new Application();
-
-        app.get("/hello", (req, res) -> {
-            if (req.isPostMethod()) {
-                res.convertTo(HttpResponse.created());
-            } else {
-                res.convertTo(HttpResponse.ok());
-            }
-        });
-        app.get("/greet", (req, res) -> {
-            StringBuilder names = extractNamesFrom(req);
-            res.convertTo(HttpResponse.ok("Hi, " + names + "!"));
-        });
-
-        app.run();
-    }
-
-    private static StringBuilder extractNamesFrom(HttpRequest httpRequest) {
-        StringBuilder names = new StringBuilder();
-        for (QueryParameter queryParameter : httpRequest.queryParameters()) {
-            if (!names.toString().isEmpty()) {
-                names.append(" and ");
-            }
-            names.append(queryParameter.value());
-        }
-        return names;
+        Main.main(new String[] {});
     }
 }

8cc22196daaa1caf66834e8e0be123b4c0e13343
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-14 17:08:04 +0100

    refactor: move routing code to client (tests)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,29 +4,21 @@ import java.net.Socket;
 import java.util.function.BiConsumer;
 
 public class Application {
-    private static final Routes routes = new Routes();
+    private final Routes routes = new Routes();
+
+    public void get(String path, BiConsumer<HttpRequest, HttpResponse> handler) {
+        routes.add(new Route(path, handler));
+    }
 
     public void run() {
-        Runnable task = Application::listenHttpAndRespond;
+        Runnable task = this::listenHttpAndRespond;
         Thread thread = new Thread(task);
         thread.start();
     }
 
-    private static void listenHttpAndRespond() {
+    private void listenHttpAndRespond() {
         try (ServerSocket serverSocket = new ServerSocket(8080)) {
             System.out.println("Server is listening on port 8080");
-
-            get("/hello", (req, res) -> {
-                if (req.isPostMethod()) {
-                    res.convertTo(HttpResponse.created());
-                } else {
-                    res.convertTo(HttpResponse.ok());
-                }
-            });
-            get("/greet", (req, res) -> {
-                StringBuilder names = extractNamesFrom(req);
-                res.convertTo(HttpResponse.ok("Hi, " + names + "!"));
-            });
 
             while (true) {
                 handleClientRequest(serverSocket);
@@ -36,11 +28,7 @@ public class Application {
         }
     }
 
-    private static void get(String path, BiConsumer<HttpRequest, HttpResponse> handler) {
-        routes.add(new Route(path, handler));
-    }
-
-    private static void handleClientRequest(ServerSocket serverSocket) throws IOException {
+    private void handleClientRequest(ServerSocket serverSocket) throws IOException {
         try (Socket socket = serverSocket.accept()) {
             HttpRequest httpRequest = readHttpRequest(socket);
             System.out.println("HTTP request received: '" + httpRequest + "'");
@@ -54,12 +42,12 @@ public class Application {
         return HttpRequest.from(reader.readLine());
     }
 
-    private static void responseToClient(Socket socket, HttpRequest httpRequest) throws IOException {
+    private void responseToClient(Socket socket, HttpRequest httpRequest) throws IOException {
         HttpResponse httpResponse = createHttpResponse(httpRequest);
         writeHttpResponse(socket, httpResponse);
     }
 
-    private static HttpResponse createHttpResponse(HttpRequest httpRequest) {
+    private HttpResponse createHttpResponse(HttpRequest httpRequest) {
         for (Route route : routes) {
             if (route.matches(httpRequest)) {
                 HttpResponse httpResponse = HttpResponse.internalServerError();
@@ -68,17 +56,6 @@ public class Application {
             }
         }
         return HttpResponse.notFound();
-    }
-
-    private static StringBuilder extractNamesFrom(HttpRequest httpRequest) {
-        StringBuilder names = new StringBuilder();
-        for (QueryParameter queryParameter : httpRequest.queryParameters()) {
-            if (!names.toString().isEmpty()) {
-                names.append(" and ");
-            }
-            names.append(queryParameter.value());
-        }
-        return names;
     }
 
     private static void writeHttpResponse(Socket socket, HttpResponse response) throws IOException {
--- src/test/java/DarwinShould.java
+++ src/test/java/DarwinShould.java
@@ -96,8 +96,32 @@ public class DarwinShould {
                 .body(equalTo("Created"));
     }
 
-
     private static void runApplication() {
-        new Application().run();
+        var app = new Application();
+
+        app.get("/hello", (req, res) -> {
+            if (req.isPostMethod()) {
+                res.convertTo(HttpResponse.created());
+            } else {
+                res.convertTo(HttpResponse.ok());
+            }
+        });
+        app.get("/greet", (req, res) -> {
+            StringBuilder names = extractNamesFrom(req);
+            res.convertTo(HttpResponse.ok("Hi, " + names + "!"));
+        });
+
+        app.run();
+    }
+
+    private static StringBuilder extractNamesFrom(HttpRequest httpRequest) {
+        StringBuilder names = new StringBuilder();
+        for (QueryParameter queryParameter : httpRequest.queryParameters()) {
+            if (!names.toString().isEmpty()) {
+                names.append(" and ");
+            }
+            names.append(queryParameter.value());
+        }
+        return names;
     }
 }

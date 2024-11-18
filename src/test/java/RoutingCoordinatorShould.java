55a7769ca9cfc9fb901afe935cbd5950fa2f477a
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-18 16:54:12 +0100

    test: does not return 404 when endpoint matches and http method is GET

--- src/test/java/RoutingCoordinatorShould.java
+++ src/test/java/RoutingCoordinatorShould.java
@@ -12,4 +12,15 @@ public class RoutingCoordinatorShould {
 
         assertThat(httpResponse.statusCode()).isEqualTo(404);
     }
+
+    @Test
+    public void does_not_return_404_when_endpoint_matches_and_http_method_is_GET() {
+        var httpRequest = HttpRequest.from("GET /known HTTP/1.1");
+        RoutingCoordinator sut = new RoutingCoordinator();
+        sut.addGetRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
+
+        HttpResponse httpResponse = sut.responseTo(httpRequest);
+
+        assertThat(httpResponse.statusCode()).isNotEqualTo(404);
+    }
 }

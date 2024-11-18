2e934615be2e6dd89820bb0f10b94d56795b3192
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-18 17:38:41 +0100

    test: several minor changes

--- src/test/java/RoutingCoordinatorShould.java
+++ src/test/java/RoutingCoordinatorShould.java
@@ -50,8 +50,8 @@ public class RoutingCoordinatorShould {
     @Test
     public void return_405_when_an_endpoint_matches_but_not_the_http_method_v1() {
         RoutingCoordinator sut = new RoutingCoordinator();
-        sut.addGetRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
-        var httpRequest = HttpRequest.from("POST /known HTTP/1.1");
+        sut.addGetRoute("/", (req, res) -> res.convertTo(HttpResponse.ok()));
+        var httpRequest = HttpRequest.from("POST / HTTP/1.1");
 
         HttpResponse httpResponse = sut.responseTo(httpRequest);
 
@@ -61,19 +61,8 @@ public class RoutingCoordinatorShould {
     @Test
     public void return_405_when_an_endpoint_matches_but_not_the_http_method_v2() {
         RoutingCoordinator sut = new RoutingCoordinator();
-        sut.addGetRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
-        var httpRequest = HttpRequest.from("DELETE /known HTTP/1.1");
-
-        HttpResponse httpResponse = sut.responseTo(httpRequest);
-
-        assertThat(httpResponse.statusCode()).isEqualTo(405);
-    }
-
-    @Test
-    public void return_405_when_an_endpoint_matches_but_not_the_http_method_v3() {
-        RoutingCoordinator sut = new RoutingCoordinator();
-        sut.addPostRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
-        var httpRequest = HttpRequest.from("GET /known HTTP/1.1");
+        sut.addPostRoute("/", (req, res) -> res.convertTo(HttpResponse.ok()));
+        var httpRequest = HttpRequest.from("DELETE / HTTP/1.1");
 
         HttpResponse httpResponse = sut.responseTo(httpRequest);
 

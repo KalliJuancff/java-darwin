b840ae0fad04b4285751ef99ec08e63a37c537fc
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-18 16:55:29 +0100

    test: does not return 404 when endpoint matches and http method is POST

--- src/test/java/RoutingCoordinatorShould.java
+++ src/test/java/RoutingCoordinatorShould.java
@@ -23,4 +23,15 @@ public class RoutingCoordinatorShould {
 
         assertThat(httpResponse.statusCode()).isNotEqualTo(404);
     }
+
+    @Test
+    public void does_not_return_404_when_endpoint_matches_and_http_method_is_POST() {
+        var httpRequest = HttpRequest.from("POST /known HTTP/1.1");
+        RoutingCoordinator sut = new RoutingCoordinator();
+        sut.addPostRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
+
+        HttpResponse httpResponse = sut.responseTo(httpRequest);
+
+        assertThat(httpResponse.statusCode()).isNotEqualTo(404);
+    }
 }

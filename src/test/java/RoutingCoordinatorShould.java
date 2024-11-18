e3b15598875592d6692c66ec98b97ec9f06fe536
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-18 16:55:59 +0100

    test: does not return 404 when endpoint matches and http method is DELETE

--- src/test/java/RoutingCoordinatorShould.java
+++ src/test/java/RoutingCoordinatorShould.java
@@ -34,4 +34,15 @@ public class RoutingCoordinatorShould {
 
         assertThat(httpResponse.statusCode()).isNotEqualTo(404);
     }
+
+    @Test
+    public void does_not_return_404_when_endpoint_matches_and_http_method_is_DELETE() {
+        var httpRequest = HttpRequest.from("DELETE /known HTTP/1.1");
+        RoutingCoordinator sut = new RoutingCoordinator();
+        sut.addDeleteRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
+
+        HttpResponse httpResponse = sut.responseTo(httpRequest);
+
+        assertThat(httpResponse.statusCode()).isNotEqualTo(404);
+    }
 }

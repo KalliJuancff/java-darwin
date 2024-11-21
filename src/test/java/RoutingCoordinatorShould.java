5154376f38e8da37f2293a373cae6e162f1a03a2
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-21 10:31:05 +0100

    test: apply several 'replace explicit type with var' in RoutingCoordinatorShould test suite

--- src/test/java/RoutingCoordinatorShould.java
+++ src/test/java/RoutingCoordinatorShould.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RoutingCoordinatorShould {
     @Test
     public void return_404_when_no_endpoint_matches() {
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         var httpRequest = HttpRequest.from("GET /unknown HTTP/1.1");
 
         HttpResponse httpResponse = sut.responseTo(httpRequest);
@@ -15,7 +15,7 @@ public class RoutingCoordinatorShould {
 
     @Test
     public void does_not_return_404_when_an_endpoint_matches_because_get_route_was_added() {
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         sut.addGetRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
         var httpRequest = HttpRequest.from("GET /known HTTP/1.1");
 
@@ -26,7 +26,7 @@ public class RoutingCoordinatorShould {
 
     @Test
     public void does_not_return_404_when_an_endpoint_matches_because_post_route_was_added() {
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         sut.addPostRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
         var httpRequest = HttpRequest.from("POST /known HTTP/1.1");
 
@@ -37,7 +37,7 @@ public class RoutingCoordinatorShould {
 
     @Test
     public void does_not_return_404_when_an_endpoint_matches_because_delete_route_was_added() {
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         sut.addDeleteRoute("/known", (req, res) -> res.convertTo(HttpResponse.ok()));
         var httpRequest = HttpRequest.from("DELETE /known HTTP/1.1");
 
@@ -49,7 +49,7 @@ public class RoutingCoordinatorShould {
 
     @Test
     public void return_405_when_an_endpoint_matches_but_not_the_http_method() {
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         sut.addGetRoute("/", (req, res) -> res.convertTo(HttpResponse.ok()));
         var httpRequest = HttpRequest.from("POST / HTTP/1.1");
 
@@ -62,7 +62,7 @@ public class RoutingCoordinatorShould {
     @Test
     public void return_500_when_user_callback_throws_an_exception() {
         final String ANY_EXCEPTION_MESSAGE = "Something went wrong";
-        RoutingCoordinator sut = new RoutingCoordinator();
+        var sut = new RoutingCoordinator();
         sut.addGetRoute("/", (req, res) -> {
             throw new RuntimeException(ANY_EXCEPTION_MESSAGE);
         });

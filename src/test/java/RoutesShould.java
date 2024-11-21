a042a4d73d6b65bf22da97423caa0e3f6944953c
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-21 15:41:43 +0100

    test: several minor changes in tests of RoutesShould

--- src/test/java/RoutesShould.java
+++ src/test/java/RoutesShould.java
@@ -4,16 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoutesShould {
     @Test
-    public void consider_no_match_when_none_route_is_not_registered() {
+    public void consider_no_match_when_path_is_not_registered() {
         assertThat(new Routes().matches(new Path("/any-route"))).isFalse();
     }
 
     @Test
-    public void consider_match_when_verb_and_path_are_registered() {
-        String path = "/any-route";
+    public void consider_match_when_path_is_registered() {
+        final String PATH = "/any-route";
+        final HttpMethod ANY_HTTP_METHOD = HttpMethod.GET;
+        Route anyRoute = new Route(new EndPoint(ANY_HTTP_METHOD, new Path(PATH)), (req, res) -> { });
         Routes sut = new Routes();
-        sut.add(new Route(new EndPoint(HttpMethod.GET, new Path(path)), (req, res) -> {}));
+        sut.add(anyRoute);
 
-        assertThat(sut.matches(new Path(path))).isTrue();
+        assertThat(sut.matches(new Path(PATH))).isTrue();
     }
 }

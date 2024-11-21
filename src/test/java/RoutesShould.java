fa035b25df97ede5668635261122f4e33d9af53a
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-11-21 15:35:33 +0100

    test: consider_match_when_verb_and_path_are_registered

--- src/test/java/RoutesShould.java
+++ src/test/java/RoutesShould.java
@@ -7,4 +7,13 @@ public class RoutesShould {
     public void consider_no_match_when_none_route_is_not_registered() {
         assertThat(new Routes().matches(new Path("/any-route"))).isFalse();
     }
+
+    @Test
+    public void consider_match_when_verb_and_path_are_registered() {
+        String path = "/any-route";
+        Routes sut = new Routes();
+        sut.add(new Route(new EndPoint(HttpMethod.GET, new Path(path)), (req, res) -> {}));
+
+        assertThat(sut.matches(new Path(path))).isTrue();
+    }
 }
